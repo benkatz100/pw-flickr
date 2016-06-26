a6f784cee8983124fa09073fdc849224a1aa9f40
hesher <ben.katz@gmail.com>
2016-06-26 13:07:37 +0300

    ongoing

--- app-sw.js
+++ app-sw.js
@@ -14,20 +14,10 @@ self.addEventListener('install', function (event) {
         './loading.gif'
       ]);
     }).then(function () {
-      console.log('finished adding all');
+      console.log('[INSTALL] finished adding all');
     })
   );
 });
-
-self.addEventListener('activate', event =>
-  event.waitUntil(caches.keys().then(
-    keys =>
-    Promise.all(keys.map(
-      key =>
-      (key !== cachesId) ? caches.delete(key) : null
-    ))
-  ))
-);
 
 let cacheResponse = request => response => {
   caches.open(cachesId).then(cache => cache.put(request, response.clone()));
@@ -47,9 +37,12 @@ self.addEventListener('fetch', function (event) {
   );
 });
 
-self.addEventListener('activate', function (event) {
-  event.waitUntil(caches.keys().then(keyList => Promise.all(keyList.map(key =>
-    (key.indexOf('v1') !== -1) ? caches.delete(key) : null
-  ))));
-
-});
+self.addEventListener('activate', event =>
+  event.waitUntil(caches.keys().then(
+    keys =>
+    Promise.all(keys.map(
+      key =>
+      (key !== cachesId) ? caches.delete(key) : null
+    ))
+  ))
+);
